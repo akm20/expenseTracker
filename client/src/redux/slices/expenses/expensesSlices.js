ca0221e6728b265a11b7d2b67e46c5629e290e3f
akm20 <106100557+akm20@users.noreply.github.com>
2022-07-17 18:50:44 +0530

    Add files via upload

--- client/src/redux/slices/expenses/expensesSlices.js
+++ client/src/redux/slices/expenses/expensesSlices.js
@@ -1,1 +1,162 @@
+import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import baseURL from "../../../utils/baseURL";
 
+// Action for redirct
+export const resetExpCreated = createAction("expense/created/reset");
+export const resetExpUpdate = createAction("expense/update/reset");
+
+//create action
+export const createExpAction = createAsyncThunk(
+  "expense/create",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    //get user token from store
+
+    const userToken = getState()?.users?.userAuth?.token;
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+    };
+    try {
+      //make http call here
+      const { data } = await axios.post(`${baseURL}/expenses`, payload, config);
+
+      //dispatch
+      dispatch(resetExpCreated());
+      return data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+//fetch all action action
+export const fetchAllExpAction = createAsyncThunk(
+  "expense/fetch",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    //get user token from store
+
+    const userToken = getState()?.users?.userAuth?.token;
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+    };
+    try {
+      //make http call here
+      const { data } = await axios.get(
+        `${baseURL}/expenses?page=${payload}`,
+        config
+      );
+      return data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+//register action
+export const updateExpAction = createAsyncThunk(
+  "expense/update",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    //get user token from store
+
+    const userToken = getState()?.users?.userAuth?.token;
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+    };
+    try {
+      //make http call here
+      const { data } = await axios.put(
+        `${baseURL}/expenses/${payload?.id}`,
+        payload,
+        config
+      );
+      dispatch(resetExpUpdate());
+      return data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+//slices
+const expenseSlices = createSlice({
+  name: "expenses",
+  initialState: {},
+  extraReducers: (builder) => {
+    // Create expense
+    builder.addCase(createExpAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    //reset action
+    builder.addCase(resetExpCreated, (state, action) => {
+      state.isExpCreated = true;
+    });
+    builder.addCase(createExpAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.expenseCreated = action?.payload;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+      state.isExpCreated = false;
+    });
+    builder.addCase(createExpAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.msg;
+      state.serverErr = action?.error?.msg;
+    });
+
+    // fetch all expenses
+    builder.addCase(fetchAllExpAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchAllExpAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.expensesList = action?.payload;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    });
+    builder.addCase(fetchAllExpAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.msg;
+      state.serverErr = action?.error?.msg;
+    });
+
+    // update expenses
+    builder.addCase(updateExpAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    //reset action
+    builder.addCase(resetExpUpdate, (state, action) => {
+      state.isExpUpdated = true;
+    });
+    builder.addCase(updateExpAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.expensesUpdated = action?.payload;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    });
+    builder.addCase(updateExpAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.msg;
+      state.serverErr = action?.error?.msg;
+    });
+  },
+});
+
+export default expenseSlices.reducer;
